6a1eca1200df7f0a394e57d4f06a67fc81d21956
RituDey <38423617+RituDey@users.noreply.github.com>
2018-04-27 15:14:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,33 @@
-const http = require('http');
+"use strict";
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-   //write a response to the client
-                                var input = 'Lenna';
-                                getData().then((output) => {
-                                
-                                                console.log(output);
-												
-                                }).catch((error) => {
-                                // If there is an error let the user know
-                                //res.setHeader('Content-Type', 'application/json');
-                                //res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
-                  });
-  res.write('hi');
-                res.end();
-				//end the response
-}).listen(8080); //the server object listens on port 8080
+const express = require("express");
+const bodyParser = require("body-parser");
 
-function getData () {
-  return new Promise((resolve, reject) => {
+const restService = express();
 
-                var GoogleSpreadsheet = require('google-spreadsheet');
-                var creds = require('./client_secret.json');
-
-                // Create a document object using the ID of the spreadsheet - obtained from its URL.
-                var doc = new GoogleSpreadsheet('1UwfGyX4NeTyds9mgXG2ldx_wwY2qCur6jhD3kqOisDc');
-
-                // Authenticate with the Google Spreadsheets API.
-                doc.useServiceAccountAuth(creds, function (err) {
-                                console.log('here');
-                  // Get all of the rows from the spreadsheet.
-                  doc.getRows(1, function (err1, rows1) {
-                                if (err1) {
-                                                console.log(err1);
-                                                reject(err1);
-                                }
-                                else {
-                                                var data1 = rows1;
-                                                var data = [];
-                                                                               
-                                                    for (let row1 in data1) {
-								var prod = {};
-								prod.product = data1[row1]['product'];
-                                                                prod.qty = data1[row1]['qty'];
-																									
-                                                            data.push(prod);
-                                                        }
-                                                        //console.log(data[0]);
-                                                        resolve(data[0]);
-                                                  
-                                                
-                                }
-                  });
-                });
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
   })
-}
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/newservice", function(req, res) {
+  var product =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.any
+      ? req.body.result.parameters.any
+      : "No products found";
+  return res.json({
+    speech: product,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
+
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
