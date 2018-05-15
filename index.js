e78aacd1f0095c6b644c14e2675732d0b2df4bd5
RituDey <38423617+RituDey@users.noreply.github.com>
2018-05-15 08:54:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -53,14 +53,14 @@ var input = req.body.result.parameters.param1.toLowerCase() ;
 				var aisleno = data1[row1]['aisle'].toLowerCase();
 				 if(input == productname && !(aisleno.includes("aisle"))){
 					prod.aisle = aisleno;
-					 prod.toString();
-					 //myString = JSON.stringify(prod);
-					//myString = myString.replace(/[{}]/g, '');
+					// prod.toString();
+					 myString = JSON.stringify(prod);
+					myString = myString.replace(/\"/g, "");
 					//string1 = string1 + data1[row1].product + " can be found in "  +  myString +  ' ; ';
 				}else if (input == productname && aisleno.includes("aisle")) {
 				       prod = aisleno;
-				       // myString = JSON.stringify(prod);
-				      // myString = myString.replace(/[{}]/g, '');
+				        myString = JSON.stringify(prod);
+				       myString = myString.replace(/\"/g, "");
 				      // string1 =  " It is available "  +  myString +  ' ; ';
 				}
 				
