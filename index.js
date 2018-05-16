3e0c50af95c9e862d60bca5a91a4ed4b0ee4d5cb
RituDey <38423617+RituDey@users.noreply.github.com>
2018-05-16 15:34:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -87,6 +87,80 @@ restService.post('/finditem', function(req, res) {
  });
 });
 
+restService.post('/getproduct', function(req, res) {
+    //console.log('=============' + req.body.result.action)
+    
+ var resp = {},
+ ret = '';
+ var GoogleSpreadsheet = require('google-spreadsheet');
+ var creds = require('./client_secret.json');
+ var doc = new GoogleSpreadsheet('1sS52vZsiJ_ZVJxuW4U1YKHHruj4mdLV4ETTsTfsQKhQ');
+ var input = req.body.result.parameters.param1.toLowerCase() ;
+ var myString = "";
+ var flag = 0;
+ doc.useServiceAccountAuth(creds, function (err) {
+  if (err) {
+   console.log(err);
+   ret = err;
+   speech = ret;
+   resp.speech = speech;
+   resp.displayText = speech;
+   return res.json(resp);    
+  }
+  else {
+   doc.getRows(1, function (err1, rows1) {
+	   console.log('here');
+    if (err1) {
+     console.log(err1);
+     ret = err1;
+     speech = ret;
+     resp.speech = speech;
+     resp.displayText = speech;
+     return res.json(resp);    
+    }
+    else {
+    
+			var data1 = rows1;
+			var string1 = "";
+	                //var output = ["Saab", "Volvo", "BMW"];
+			for (let row1 in data1) {
+				var prod = {};
+				var productname = data1[row1].product.toLowerCase();
+				productname = productname.replace("&","and");
+				var aisleno = data1[row1]['aisle'].toLowerCase();
+				 if((input.replace("&","and")) == productname && !(aisleno.includes("aisle"))){
+					flag++;
+					prod = aisleno;
+					myString = JSON.stringify(prod);
+					myString = myString.replace(/\"/g, "");
+					myString = "aisle " + myString ;
+					string1 = string1 + " It is available in " +  myString + ';';
+					
+				}else if ((input.replace("&","and")) == productname && aisleno.includes("aisle")) {
+				       flag++;
+				       prod = aisleno;
+				       myString = JSON.stringify(prod);
+				       myString = myString.replace(/\"/g, "");
+				       string1 = string1 + " You can find it " +  myString +  ';'; 
+				}
+				
+		}
+			if(flag <= 0){
+			    string1 = string1 + "Currently this product is not available in our store " + ';';
+			}		
+	       return res.json({
+                    
+			"speech": string1,
+			"source": "webhook-echo-one",
+		       
+			
+         
+                });
+    }
+   });
+  }
+ });
+});
 
  
 restService.listen(process.env.PORT || 8000, function() {
